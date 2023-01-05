467c515c9cf7aa0a203c6fdc79e779f97b03137c
sudips413 <sudips413@gmsil.com>
2023-01-05 13:20:07 +0545

    Added arrow-function with filter,map and reduce

---
+++ javascript/arrow-function.js
@@ -0,0 +1,18 @@
+//arrow function is called anonymous function like lambda function in python
+//arrow function is used to write less code
+
+const addTwoVar = (a,b)=>a+b
+console.log(addTwoVar("Sudip"," Shrestha"))
+console.log(addTwoVar(2,3))
+
+//arrow function with no parameter
+const sayHello=()=>console.log("Hello")
+sayHello()
+//arrow function with filter,map and reduce
+const arr=[2,4,6,8,10]
+//prints all number divisbile by 3
+console.log(arr.filter((num)=>num%3==0))
+//print square of all number
+console.log(arr.map((num,index)=>num**2))
+//print sum of all number
+console.log(arr.reduce((total,num)=>total+num))
