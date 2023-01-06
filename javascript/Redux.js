6c0034285158e27a83df1f793a276e6c2241da21
sudips413 <sudips413@gmsil.com>
2023-01-06 20:41:36 +0545

    Added python changed file

---
+++ javascript/Redux.js
@@ -0,0 +1,21 @@
+const ADD_NOTE = 'ADD_NOTE';
+const notesReducer = (state = 'Initial State', action) => {
+  switch(action.type) {
+  
+    case "ADD_NOTE":
+    return action.text
+
+    default:
+      return state;
+  }
+};
+const addNoteText = (note) => {  
+  return{
+    type: ADD_NOTE,
+    text:note
+  }
+};
+const store = Redux.createStore(notesReducer);
+console.log(store.getState());
+store.dispatch(addNoteText('Hello!'));
+console.log(store.getState());
--- python/regex_example.py
+++ python/regex_example.py
@@ -4,9 +4,9 @@ import re
 def is_valid_email(email):
 	regex=r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 
-	if re.match(regec,email):
+	if re.match(regex,email):
 		return True
 	else:
 		return False
 
-print(is_valid_email(sampleEmail@example.com))
+print(is_valid_email("sampleEmail@example.com"))
